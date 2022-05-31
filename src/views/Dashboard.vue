287fd07cd13250d600cde46c8e375c232406f036
Vatsalya-singhi <vatsalya.singhi4316@gmail.com>
2022-05-31 18:06:44 +0530

    dashboard update

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,11 +1,41 @@
 <template>
-  <div>Dashboard-Page</div>
+  <div class="dashboard">
+
+    <v-subheader class="grey--text darken-5">Dashboard</v-subheader>
+
+    <v-container fluid>
+      <v-row class="mb-5" align-content="center" justify="space-around" dense>
+        <v-col md="2" xs="12">
+          <Tile title="Total Courses" count="50" type="neutral" />
+        </v-col>
+
+        <v-col md="2" xs="12">
+          <Tile title="Total Students" count="30" type="pass" />
+        </v-col>
+
+        <v-col md="2" xs="12">
+          <Tile title="Total Questions" count="20" type="fail" />
+        </v-col>
+
+        <v-col md="2" xs="12">
+          <Tile title="Total X" count="60/100" type="neutral" />
+        </v-col>
+
+        <v-col md="2" xs="12">
+          <Tile title="Total Y" count="3.7" type="pass" />
+        </v-col>
+        
+      </v-row>
+      <v-divider></v-divider>
+    </v-container>
+  </div>
 </template>
 
 <script>
+import Tile from "../components/dashboard/Tile.vue";
 export default {
   name: "Home-Page",
-  components: {},
+  components: { Tile },
 };
 </script>
 
--- src/views/Pages.vue
+++ src/views/Pages.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <NavBar />
-    <router-view />
+    <div class="mx-4">
+      <router-view />
+    </div>
   </div>
 </template>
 
 <script>
+/* eslint-disable vue/multi-word-component-names */
 import NavBar from "../components/common/NavBar.vue";
 export default {
-  name: "Dashboard-Page",
+  name: "Pages",
   components: {
     NavBar,
   },
